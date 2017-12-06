f8018cb59d019a453a6023dd5543685bc6f20a66
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2017-12-06 20:01:35 +0100

    added logs for all the virtual methods

--- common/IndiClient.cpp
+++ common/IndiClient.cpp
@@ -7,7 +7,7 @@ IndiClient::IndiClient(QObject *parent) : INDI::BaseClientQt(parent)
 
 bool IndiClient::connect(const QString &host, int port)
 {
-    emit message("(II) connecting to host " + host + " port " + QString::number(port));
+    emit message("connecting to host " + host + " port " + QString::number(port));
     setServer(host.toStdString().c_str(), port);
     bool ans = connectServer();
 
@@ -24,49 +24,58 @@ bool IndiClient::isConnected() const
 
 void IndiClient::disconnect()
 {
-    emit message("(II) disconnecting from host");
+    emit message("disconnecting from host");
     disconnectServer();
 }
 
-
 void IndiClient::newDevice(INDI::BaseDevice *dp)
 {
+    emit message("(INDI) newDevice");
 }
 
 void IndiClient::removeDevice(INDI::BaseDevice *dp)
 {
+    emit message("(INDI) removeDevice");
 }
 
 void IndiClient::newProperty(INDI::Property *property)
 {
+    emit message("(INDI) newProperty");
 }
 
 void IndiClient::removeProperty(INDI::Property *property)
 {
+    emit message("(INDI) removeProperty");
 }
 
 void IndiClient::newBLOB(IBLOB *bp)
 {
+    emit message("(INDI) newBLOB");
 }
 
 void IndiClient::newSwitch(ISwitchVectorProperty *svp)
 {
+    emit message("(INDI) newSwitch");
 }
 
 void IndiClient::newNumber(INumberVectorProperty *nvp)
 {
+    emit message("(INDI) newNumber");
 }
 
 void IndiClient::newText(ITextVectorProperty *tvp)
 {
+    emit message("(INDI) newText");
 }
 
 void IndiClient::newLight(ILightVectorProperty *lvp)
 {
+    emit message("(INDI) newLight");
 }
 
 void IndiClient::newMessage(INDI::BaseDevice *dp, int messageID)
 {
+    emit message("(INDI) newMessage");
 }
 
 void IndiClient::serverConnected()
